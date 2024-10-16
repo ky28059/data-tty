c8391db31a40f7918e45463244d041316d8a015f
tillvit <lucas.j.tan@gmail.com>
2024-10-16 19:29:55 -0400

    Send window size

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import os
+import signal
 import socket
 import struct
 import subprocess
@@ -11,7 +13,7 @@ conn.connect(("localhost", int(sys.argv[1])))
 
 
 id = int(subprocess.run(['tty'], stdout=subprocess.PIPE).stdout[9:-1].decode())
-conn.send(b'\x01' + struct.pack("<i", id))
+conn.send(b'\x01' + struct.pack("<I", id))
 
 def getch():
     fd = sys.stdin.fileno()
@@ -29,6 +31,14 @@ def get_input():
             break
         conn.send(b'\x04' + int.to_bytes(ord(ch), 1, "little"))
     conn.close()
+
+def handle_resize():
+    size = os.get_terminal_size()
+    print(f"Terminal resized to {size.columns}x{size.lines}")
+    conn.send(b'\x02' + struct.pack("<I", size.columns) + struct.pack("<I", size.lines))
+
+signal.signal(signal.SIGWINCH, handle_resize)
+handle_resize()
 
 input_thread = threading.Thread(target=get_input, daemon=True)
 input_thread.start()
