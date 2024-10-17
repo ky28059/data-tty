8c5eaab7b27a9ffc35960a5c49c9a4d227fa35a0
tillvit <lucas.j.tan@gmail.com>
2024-10-17 16:51:33 -0400

    Better screen for negatives

--- util/screen.py
+++ util/screen.py
@@ -16,11 +16,21 @@ class Screen:
         return out
 
     def write_text(self, text: str, x: int, y: int):
+        if x < 0:
+            x += self.width
+        if y < 0:
+            y += self.height
         self.lines[y][x:len(text)+x] = text.encode()
 
     def horizontal_line(self, y: int, x_start: int = 0, x_end: int = None, char = b"-"):
         if x_end is None:
             x_end = self.width
+        if x_start < 0:
+            x_start += self.width
+        if x_end < 0:
+            x_end += self.width
+        if y < 0:
+            y += self.height
 
         self.lines[y][x_start:x_end+1] = char * (x_end - x_start + 1)
 
@@ -29,10 +39,24 @@ class Screen:
             y_start = 0
         if y_end is None:
             y_end = self.height
+        if x < 0:
+            x += self.width
+        if y_start < 0:
+            y_start += self.height
+        if y_end < 0:
+            y_end += self.height
 
         for y in range(y_start, y_end):
-            self.lines[y][x] = int(char)
+            self.lines[y][x] = ord(char)
 
     def fill_rect(self, x_start: int, y_start: int, x_end: int, y_end: int, char = b"-"):
+        if x_start < 0:
+            x_start += self.width
+        if x_end < 0:
+            x_end += self.width
+        if y_start < 0:
+            y_start += self.height
+        if y_end < 0:
+            y_end += self.height
         for y in range(y_start, y_end+1):
             self.lines[y][x_start:x_end+1] = char * (x_end-x_start + 1)
