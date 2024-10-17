61db86b2d1962b94a96a1ac0b060993633699a3d
tillvit <lucas.j.tan@gmail.com>
2024-10-16 22:20:04 -0400

    Update screen.py

--- util/screen.py
+++ util/screen.py
@@ -17,3 +17,18 @@ class Screen:
 
     def write_text(self, text: str, x: int, y: int):
         self.lines[y][x:len(text)+x] = text.encode()
+
+    def horizontal_line(self, y: int, x_start: int = 0, x_end: int = None, char = b"-"):
+        if x_end is None:
+            x_end = self.width
+
+        self.lines[y][x_start:x_end] = char * (x_end - x_start)
+
+    def vertical_line(self, x: int, y_start: int = 0, y_end: int = None, char = b"-"):
+        if y_start is None:
+            y_start = 0
+        if y_end is None:
+            y_end = self.height
+
+        for y in range(y_start, y_end):
+            self.lines[y][x] = char
